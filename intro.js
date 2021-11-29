fe0c23c42ea373be4410719c972eccb31eaec8f9
Steven <crisstevent@gmail.com>
2021-11-29 21:37:06 +0200

    Javascript comments

--- intro.js
+++ intro.js
@@ -1,15 +1,18 @@
+//JS Connor Bailey poolt (youtube.com/watch?v=2ak37WrbSDg),
+//Kood animeerib lehe "Sissejuhatus" alguses olena n.ö. "intro".
 let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header');
 let logoSpan = document.querySelectorAll('.logo');
 
 window.addEventListener("DOMContentLoaded", ()=> {
+    //Funktsioon kus logo tuleb nähtavale.
     setTimeout(()=>{
         logoSpan.forEach((span, idx)=> {
             setTimeout(()=>{
                 span.classList.add('active');
             }, (idx + 1 ) * 400)
         });
-
+        //Funktsioon kus logo kaob.
         setTimeout(()=>{
             logoSpan.forEach((span, idx)=>{
 
@@ -19,6 +22,7 @@ window.addEventListener("DOMContentLoaded", ()=> {
                 }, (idx + 1) * 50)
             })
         }, 2000);
+        //Hall osa liigub lehelt üles, nähtavale tuleb lehe sisu.
         setTimeout(()=>{
             intro.style.top = '-100vh';
         }, 2300)
